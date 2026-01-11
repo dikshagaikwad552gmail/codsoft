e105b29a3f036e5e568f4e37ea47fca8db8ec3a0
diksha <dikshagaikead552@gmail.com>
2026-01-12 00:05:19 +0530

    Added new mini project

---
+++ mini project/script.js
@@ -0,0 +1,15 @@
+function addTask() {
+    var input = document.getElementById("taskInput");
+    var task = input.value;
+
+    if (task === "") {
+        alert("Please enter a task");
+        return;
+    }
+
+    var li = document.createElement("li");
+    li.innerText = task;
+
+    document.getElementById("taskList").appendChild(li);
+    input.value = "";
+}
